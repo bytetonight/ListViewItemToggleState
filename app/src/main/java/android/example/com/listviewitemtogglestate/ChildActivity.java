78822b8f20524fdea890c4aa05d4f403d4f4bce2
bytetonight <isisteam@web.de>
2018-04-17 18:40:54 +0200

    Added a back navigation as requested from a Student : The back navigation was to happen on the click of a Button

--- app/src/main/java/android/example/com/listviewitemtogglestate/ChildActivity.java
+++ app/src/main/java/android/example/com/listviewitemtogglestate/ChildActivity.java
@@ -1,8 +1,11 @@
 package android.example.com.listviewitemtogglestate;
 
 import android.content.Intent;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -12,6 +15,7 @@ public class ChildActivity extends AppCompatActivity {
 
     private ListItem listItem;
     private TextView simpleText;
+    private Button backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +23,7 @@ public class ChildActivity extends AppCompatActivity {
         setContentView(R.layout.activity_child);
         setTitle("ChildActivity");
         simpleText = findViewById(R.id.simpleText);
+        backButton = findViewById(R.id.navigateBack);
 
         Intent senderIntent = getIntent();
         if (senderIntent.hasExtra(Global.KEY_EXTRA_DATA)) {
@@ -28,5 +33,15 @@ public class ChildActivity extends AppCompatActivity {
                     getString(R.string.placeholder_text, listItem.getIdString())
             );
         }
+
+        backButton.setOnClickListener(myOnClickListener);
     }
+
+
+    private View.OnClickListener myOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            NavUtils.navigateUpFromSameTask(ChildActivity.this);
+        }
+    };
 }
